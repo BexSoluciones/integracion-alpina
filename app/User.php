bf446c8b8e1daf35171a2a830c227e6ea29050fe
Albert Pacheco <apachecoo110@gmail.com>
2021-04-30 14:17:15 -0500

    Se traduce mensaje de olvide contraseña

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\RecuperarContrasenaNotification;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
@@ -37,4 +38,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+     /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new RecuperarContrasenaNotification($token));
+    }
 }
